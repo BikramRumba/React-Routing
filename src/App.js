8bdd999e48113f07e0cdcb22d6a02082618d7f5d
BikramRumba <rumbikram123@gmail.com>
2022-01-30 15:36:08 -0600

    added routing path

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import About from './components/About';
 import './App.css';
 import Nav from './components/Nav';
 import Shop from './components/Shop';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
+
     <div className="App">
       <Nav/>
-      <About />
-      <Shop/>
+      <Switch>
+
+      <Route  path="/" exact component={Home} />
+     <Route path= "/about" component={About} />
+     <Route path= "/shop" component={Shop} />
+     </Switch>
+
     </div>
+    </Router>
   );
 }
+
+
+const Home = () => (
+  <div>
+    <h1>Homepage</h1>
+  </div>
+)
 
 export default App;
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+
+/*  --> useState kind of holds the information that we're
+  getting from the API. AND
+      --useEffect basically runs that fetch call when our component mounts.
+  */
 
 function Shop() {
+  useEffect(()=> {
+    fetchItem();
+  }, []);
+
+  const fetchItem = async() => {
+    const data = await fetch(
+      'http https://swapi.dev/api/people/1/');
+      
+    console.log(data);
+
+  };
+
   return <div>
       <h1>Shop Page</h1>
   </div>;
